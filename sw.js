9db05542897ef0645f4a36151f5cc6bcb2c1bf13
mac <mac@JWS---Pato.local>
2026-08-30 00:40:06 +0100

    Make FinanceOS a PWA: add manifest, service worker, icons, Apple meta tags, offline support

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'financeos-v1';
+const APP_SHELL = [
+  './index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
